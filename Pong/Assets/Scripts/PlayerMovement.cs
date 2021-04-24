0b303ed61973b723235850efd7d5ca6a9cd6f65d
Ali Poorsolaymani <alips96@gmail.com>
2021-04-24 15:36:10 +0200

    Player movement

---
+++ Pong/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D myrb;
+    private Vector3 mousePos;
+
+    private void Start()
+    {
+        myrb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+    }
+
+    private void FixedUpdate()
+    {
+        myrb.MovePosition(new Vector2(7, mousePos.y));
+    }
+}
