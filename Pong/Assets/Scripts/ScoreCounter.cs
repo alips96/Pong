3b4425c915185e16f8d87c3935375cbbc0f7e505
Ali Poorsolaymani <alips96@gmail.com>
2021-04-25 10:01:54 +0200

    Score Counter
    Incrementing the score every time the players hit the target.

---
+++ Pong/Assets/Scripts/ScoreCounter.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreCounter : MonoBehaviour
+{
+    [SerializeField] private Text opponentScoreText;
+    [SerializeField] private Text playerScoreText;
+
+    private byte playerScore, opponentScore = 0;
+
+    private void OnTriggerEnter2D(Collider2D bonusBar)
+    {
+        if (bonusBar.CompareTag("OppBar")) //Player Scored!
+        {
+            playerScore++;
+            playerScoreText.text = playerScore.ToString();
+
+        }
+        else
+        {
+            opponentScore++;
+            opponentScoreText.text = opponentScore.ToString();
+        }
+    }
+}
