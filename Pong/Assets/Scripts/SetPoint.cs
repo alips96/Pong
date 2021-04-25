dce446f48cb30780becad19f6c2f39ce6eaee6c0
Ali Poorsolaymani <alips96@gmail.com>
2021-04-25 11:06:12 +0200

    Set Point
    Now the game fully functions except the opponent movement :D

---
+++ Pong/Assets/Scripts/SetPoint.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class SetPoint : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        transform.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        transform.position = Vector3.zero;
+        StartCoroutine(WaitForSeconds(2, other.tag));
+    }
+
+    private IEnumerator WaitForSeconds(int seconds, string tag)
+    {
+        yield return new WaitForSeconds(seconds);
+
+        if (tag.Equals("OppBar"))
+        {
+            transform.GetComponent<Rigidbody2D>().velocity = new Vector2(8, 1);
+        }
+        else
+        {
+            transform.GetComponent<Rigidbody2D>().velocity = new Vector2(-8, 1);
+        }    
+
+    }
+}
