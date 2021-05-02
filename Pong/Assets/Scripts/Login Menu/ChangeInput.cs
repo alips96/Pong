aee33ff6c14249348d02c577b29f0786dfd3a5e7
Ali Poorsolaymani <alips96@gmail.com>
2021-05-02 18:57:12 +0200

    Login Menu
    Prototype of the login menu.

---
+++ Pong/Assets/Scripts/Login Menu/ChangeInput.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ChangeInput : MonoBehaviour
+{
+    EventSystem system;
+    [SerializeField] private Selectable firstInput;
+    [SerializeField] private Button submitButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        system = EventSystem.current;
+        firstInput.Select();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Tab) && Input.GetKey(KeyCode.LeftShift))
+        {
+            Selectable previous = system.currentSelectedGameObject.GetComponent<Selectable>().FindSelectableOnUp();
+
+            if(previous != null)
+            {
+                previous.Select();
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            Selectable next = system.currentSelectedGameObject.GetComponent<Selectable>().FindSelectableOnDown();
+
+            if (next != null)
+            {
+                next.Select();
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.Return))
+        {
+            submitButton.onClick.Invoke();
+            Debug.Log("Button Passed!");
+        }
+    }
+}
