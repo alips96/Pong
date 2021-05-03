27653b3f0b0b8332d637ec9a2d79735d6e8473a1
Ali Poorsolaymani <alips96@gmail.com>
2021-05-03 08:47:14 +0200

    PlayFab Setup
    Setting up playFab and simple logging in.

---
+++ Pong/Assets/Scripts/PlayFab/PlayFabManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using PlayFab;
+using PlayFab.ClientModels;
+
+public class PlayFabManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Login();
+    }
+
+    private void Login()
+    {
+        var request = new LoginWithCustomIDRequest
+        {
+            CustomId = SystemInfo.deviceUniqueIdentifier,
+            CreateAccount = true
+        };
+
+        PlayFabClientAPI.LoginWithCustomID(request, OnSuccess, OnError);
+    }
+
+    private void OnError(PlayFabError error)
+    {
+        Debug.Log("Error while loggin in/creating account :(");
+        Debug.Log(error.GenerateErrorReport());
+    }
+
+    private void OnSuccess(LoginResult result)
+    {
+        Debug.Log("Successful Login/account create!");
+    }
+}
