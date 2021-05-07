365e2660c3851e29d045fa5f5671c408fbdaf445
Ali Poorsolaymani <alips96@gmail.com>
2021-05-07 10:24:56 +0200

    Single Player Gameplay
    Score counter + game over + replay

---
+++ Pong/Assets/Scripts/SinglePlayer/SP_BallMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class SP_BallMovement : MonoBehaviour
+{
+    private Rigidbody2D myrb;
+    private Vector3 lastVelocity;
+
+    [SerializeField] private float ballInitialSpeed = 5f;
+    [SerializeField] private float acceleration = 0.3f;
+    [SerializeField] private float maxSpeed = 15f;
+
+    private void Start()
+    {
+        myrb = GetComponent<Rigidbody2D>();
+        myrb.velocity = new Vector2(ballInitialSpeed, Random.Range(-2f, 2f));
+    }
+
+    private void Update()
+    {
+        lastVelocity = myrb.velocity;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        float speed = lastVelocity.magnitude;
+
+        Vector3 myDirection = Vector3.Reflect(lastVelocity.normalized, collision.contacts[0].normal);
+        Vector2 myVelocity = myDirection * Mathf.Max(speed, 0);
+
+        if (lastVelocity.x * myDirection.x < 0) //if ball hits the players, because it toggles direction :)
+        {
+            myVelocity += myDirection.normalized.x * new Vector2(acceleration, 0);
+
+            if (Mathf.Abs(myVelocity.x) > maxSpeed) //if it reaches max speed.
+            {
+                myVelocity = new Vector2(maxSpeed * myDirection.normalized.x, myVelocity.y);
+            }
+        }
+
+        myrb.velocity = myVelocity;
+    }
+}
---
+++ Pong/Assets/Scripts/SinglePlayer/SP_RestartLevel.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SP_RestartLevel : MonoBehaviour
+{
+    [SerializeField] private PlayerMovement playerMovement;
+    [SerializeField] private Text scoreText;
+
+    [SerializeField] private float ballInitialSpeed = 5f;
+
+    public void ResumeGame()
+    {
+        playerMovement.enabled = true;
+        scoreText.text = "0";
+        GetComponent<Rigidbody2D>().velocity = new Vector2(ballInitialSpeed, Random.Range(-2f, 2f));
+    }
+}
