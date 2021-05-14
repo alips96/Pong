18379bdabac9517e000000f6f1fac7a030fba400
Ali Poorsolaymani <alips96@gmail.com>
2021-05-14 11:05:04 +0200

    Setting NickNames
    Now player names are displayed in the match.

---
+++ Pong/Assets/Scripts/Multiplayer/NamesManager.cs
@@ -0,0 +1,33 @@
+﻿using Photon.Pun;
+using System.Collections.Generic;
+using Photon.Realtime;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NamesManager : MonoBehaviour
+{
+    [SerializeField] private Text playerText;
+    [SerializeField] private Text opponentText;
+
+    private Player[] playersArr;
+
+    private void Start()
+    {
+        playersArr = PhotonNetwork.PlayerList;
+
+        if (playersArr.Length == PhotonNetwork.CurrentRoom.MaxPlayers)
+        {
+            AssignUINames();
+        }
+        else
+        {
+            Debug.LogError("Maximum room capacity is not reached:");
+        }
+    }
+
+    private void AssignUINames()
+    {
+        playerText.text = playersArr[0].NickName;
+        opponentText.text = playersArr[1].NickName;
+    }
+}
