0f2a9088aaab3f7db9f02a539bacd5f18d1b19a1
Ali Poorsolaymani <alips96@gmail.com>
2021-05-20 19:37:26 +0200

    Player Stats
    Win count and lose count are stored on the server.

---
+++ Pong/Assets/Scripts/PlayFab/PlayerStats.cs
@@ -0,0 +1,129 @@
+﻿using UnityEngine;
+using Photon.Pun;
+using ExitGames.Client.Photon;
+using System.Collections.Generic;
+using PlayFab;
+using PlayFab.ClientModels;
+using System;
+
+public class PlayerStats : MonoBehaviour
+{
+    private int lossCount, winCount;
+    private bool hasMasterPlayerWon;
+
+    private void OnEnable()
+    {
+        PhotonNetwork.NetworkingClient.EventReceived += GetPlayerData;
+    }
+
+    private void OnDisable()
+    {
+        PhotonNetwork.NetworkingClient.EventReceived -= GetPlayerData;
+    }
+
+    private void GetPlayerData(EventData obj)
+    {
+        if (!obj.Code.Equals(2)) //game over event
+            return;
+
+        hasMasterPlayerWon = (bool)obj.CustomData;
+        PlayFabClientAPI.GetUserData(new GetUserDataRequest(), OnPlayerDataReceived, OnError);
+    }
+
+    private void OnPlayerDataReceived(GetUserDataResult result)
+    {
+        lossCount = GetLossCount(result);
+        winCount = GetWinCount(result);
+
+        SetWinner();
+    }
+
+    private void SetWinner()
+    {
+        bool isMasterClient = PhotonNetwork.IsMasterClient;
+
+        if (hasMasterPlayerWon)
+        {
+            if (isMasterClient)
+            {
+                IncrementWonCount();
+            }
+            else
+            {
+                IncrementLoseCount();
+            }
+        }
+        else
+        {
+            if (isMasterClient)
+            {
+                IncrementLoseCount();
+            }
+            else
+            {
+                IncrementWonCount();
+            }
+        }
+    }
+
+    private void IncrementLoseCount()
+    {
+        var request = new UpdateUserDataRequest
+        {
+            Data = new Dictionary<string, string>
+            {
+                {"Lose", (++lossCount).ToString() }
+            }
+        };
+
+        PlayFabClientAPI.UpdateUserData(request, OnLostDataSent, OnError);
+    }
+
+    private void IncrementWonCount()
+    {
+        var request = new UpdateUserDataRequest
+        {
+            Data = new Dictionary<string, string>
+            {
+                {"Win", (++winCount).ToString() }
+            }
+        };
+
+        PlayFabClientAPI.UpdateUserData(request, OnWinCountSent, OnError);
+    }
+
+    private void OnWinCountSent(UpdateUserDataResult result)
+    {
+        Debug.Log("Player win count incremented successfully");
+    }
+
+    private int GetWinCount(GetUserDataResult result)
+    {
+        if (result.Data.ContainsKey("Win"))
+        {
+            return Convert.ToInt32(result.Data["Win"].Value);
+        }
+
+        return 0;
+    }
+
+    private int GetLossCount(GetUserDataResult result)
+    {
+        if (result.Data.ContainsKey("Lose"))
+        {
+            return Convert.ToInt32(result.Data["Lose"].Value);
+        }
+
+        return 0;
+    }
+
+    private void OnLostDataSent(UpdateUserDataResult result)
+    {
+        Debug.Log("Player loss count incremented successfully");
+    }
+
+    private void OnError(PlayFabError error)
+    {
+        Debug.LogError(error.GenerateErrorReport());
+    }
+}
