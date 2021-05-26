f626f7770a79222dc0fd855dac726a5a41f48168
Ali Poorsolaymani <alips96@gmail.com>
2021-05-26 18:42:53 +0200

    Discord Login Message
    As soon as the players log in or register, a message pops up on the discord server.

---
+++ Pong/Assets/Scripts/DiscordLogin.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using PlayFab;
+using PlayFab.ClientModels;
+
+public class DiscordLogin : MonoBehaviour
+{
+    private PlayFabMaster playFabMaster;
+
+    void OnEnable()
+    {
+        SetInitialReferences();
+
+        playFabMaster.EventUserLoggedIn += DisplayLoginMessage;
+    }
+
+    private void OnDisable()
+    {
+        playFabMaster.EventUserLoggedIn -= DisplayLoginMessage;
+    }
+
+    private void DisplayLoginMessage(string name)
+    {
+        var request = new ExecuteCloudScriptRequest
+        {
+            FunctionName = "userLoggedIn",
+            FunctionParameter = new
+            {
+                playerName = name
+            }
+        };
+
+        PlayFabClientAPI.ExecuteCloudScript(request, OnMessageDisplayed, OnError);
+    }
+
+    private void OnMessageDisplayed(ExecuteCloudScriptResult result)
+    {
+        Debug.Log("Login message showed on discord!");
+    }
+
+    private void OnError(PlayFabError error)
+    {
+        Debug.LogError(error.GenerateErrorReport());
+    }
+
+    private void SetInitialReferences()
+    {
+        playFabMaster = transform.parent.GetComponent<PlayFabMaster>();
+    }
+}
