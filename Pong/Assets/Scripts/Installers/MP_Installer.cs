b903c47ff0a28ea26b3349988f073e945be41fe4
Ali Poorsolaymani <alips96@gmail.com>
2021-10-08 14:35:41 +0200

    MP Score Handler Update
    Strategy pattern as well as dependency injection for MVC added.

---
+++ Pong/Assets/Scripts/Installers/MP_Installer.cs
@@ -0,0 +1,14 @@
+using Pong.General;
+using Zenject;
+
+namespace Pong.MP
+{
+    public class MP_Installer : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+            Container.Bind<EventMaster>().AsSingle();
+            Container.Bind<IScoreHandler_MP>().To<ScoreHandlerModel>().AsSingle();
+        }
+    }
+}
